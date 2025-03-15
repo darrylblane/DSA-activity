5ab41a55e9cc69a1656778ac39a8e473f24e08d3
darrylblane <darylvillanueva66@gmail.com>
2025-03-15 16:32:26 +0800

    activity 2 part 1

--- villanueva_darrylblane_lab2.cpp
+++ villanueva_darrylblane_lab2.cpp
@@ -1,56 +1,49 @@
 #include <iostream>
-
 using namespace std;
 
 typedef struct Node{
-    string songName;
-    Node  *link;
+        string songName;
+        Node *link;
 }Node;
 
-Node *createNode(string data){
-    Node *newNode = new Node;
+Node* createNode(string data) {
+    Node* newNode = new Node;
     newNode->songName = data;
     newNode->link = NULL;
-
     return newNode;
 }
-
-void traverse(Node *head){
+void traverse (Node *head){
     Node *temp = new Node;
     temp = head;
     
     cout << "My Playlist" <<endl;
-    while(temp != NULL){
-        cout << temp->songName<< "->"<<endl;
+    while(temp !=NULL){
+        cout << temp->songName<< "->" <<endl;
         if(temp->link == NULL){
-            cout << "NULL"<<endl;
-        }
-        
-        temp = temp->link;
+            cout << "NULL" << endl;
     }
+    temp = temp->link;
 }
-
+}
 Node *insertAtEnd(string data, Node *head){
     if(head == NULL){
         Node *newNode = createNode(data);
         head = newNode;
-        cout << "A new node has been inserted at the end \n" << endl;
+        cout << "A new node has been insterted at the end \n" << endl;
         return head;
     }
     Node *temp = new Node;
     temp = head;
     
-    while(temp->link != NULL){
+    while (temp->link != NULL){
         temp = temp->link;
     }
-    
     Node *newNode = createNode(data);
     temp->link = newNode;
     
-    cout << "A new node has been inserted at the end \n" << endl;
+    cout << "A new node has been insterted at the end \n" << endl;
     return head;
 }
-
 Node *insertAtBeginning(string data, Node *head){
     Node *newNode = createNode(data);
     newNode->link = head;
@@ -58,17 +51,15 @@ Node *insertAtBeginning(string data, Node *head){
     head = newNode;
     
     cout << "A new node has been inserted at the beginning \n" <<endl;
-    
     return head;
 }
-
 string insertAfter(string after, string data, Node *head){
-    Node *temp = new Node;
-    temp = head;
+    Node *temp = new Node; 
+    temp = head; 
     
-    while(temp->songName.compare(after) != 0){
+        while(temp->songName.compare(after) != 0){
         if(temp == NULL){
-            return "No such exist, please try again later.";
+            return "No such song exist, please try again.";
         }
         
         temp = temp->link;
@@ -77,29 +68,99 @@ string insertAfter(string after, string data, Node *head){
     newNode->link = temp->link;
     temp->link = newNode;
     
-    return "An new node has been inserted after " + after + "\n";
+    return "An new node has been added after" + after + "\n";
 }
+string deleteAtEnd(Node *head){
+    if(head == NULL){ 
+        return "The linked list is empty \n";
+    }
 
-int main(){
-    Node *head = createNode("Hit'Em up by 2pac");
+    if(head->link == NULL){ 
+        delete head;
+        return "The head has been deleted \n";
+    }
+
+    Node *temp = new Node; 
+    temp = head; 
+
+    while(temp->link->link != NULL){ 
+        temp = temp->link;
+    }
+
+    temp->link = NULL; 
+
+    return "A node has been deleted at the end \n";
+}
+Node *deleteFromBeginning(Node *head){
+    if(head == NULL){
+        cout << "The linked list is empty \n" << endl;
+        return NULL;
+    }
     
+    if(head->link == NULL){
+        delete head;
+    }
+    head = head->link;
+    
+    cout << "A node has been delete from the beginning \n" << endl;
+    
+    return head;
+}
+Node *deleteFromGivenNode(string givenNode, Node *head){
+    if(head == NULL){
+        cout << "The Node" + givenNode + " has been deleted. \n" << endl;
+        return head;
+    }
+    Node *temp = new Node;
+    Node *next = new Node;
+    temp = head;
+    next = temp->link;
+    
+    while(next->songName.compare(givenNode) !=0){
+        if(temp == NULL){
+            cout << "No Such node exist. \n" << endl;
+            return head;
+    }
+    next = next->link;
+    temp = temp->link;
+}
+    temp->link = next->link;
+    cout << "The Node " + givenNode + " has been deleted. \n" << endl;
+    return head;
+}
+int main()
+{
+    Node *head = createNode ("Sanctuary by Joji");
+    traverse(head);
+    head = insertAtEnd("Hit em up By 2pac", head);
+    traverse(head);
     head = insertAtEnd("It Wasn't Me by Shaggy", head);
-    head = insertAtEnd("Family Affair by Mary J.Blige", head);
+    traverse(head);
     head = insertAtEnd("Do For Love by 2pac", head);
-    head = insertAtEnd("Without Me by Eminem", head);
-    
-    head = insertAtBeginning("Easy by Mac ayres", head);
-    head = insertAtBeginning("Can We Make love by Keith Sweat", head);
-    head = insertAtBeginning("You are Not Alone by Michael Jackson", head);
-    head = insertAtBeginning("Loose by Daniel Caesar", head);
+    traverse(head);
+    head = insertAtBeginning ("Without Me by Eminem", head);
+    traverse(head);
+    head = insertAtBeginning ("Can We Make love by Keith Sweat", head);
+    traverse(head);
+    head = insertAtBeginning ("You are Not Alone by Michael Jackson", head);
+    traverse(head);
+    head = insertAtBeginning ("Loose by Daniel Caesar", head);
+    traverse(head);
+    cout << insertAfter("Without Me by Eminem","Ikaw lang by Nobita", head);
+    traverse(head);
+    cout << insertAfter("Can We Make love by Keith Sweat","Lihim by Arthur Miguel", head);
+    traverse(head);
+    cout << insertAfter("You are Not Alone by Michael Jackson","uhaw by dilaw", head);
     traverse(head);
     
-    insertAfter ("Ikaw lang by nobita", "You Are Not Alone by Michael Jackson", head);
-    insertAfter ("Lihim by Arthur miguel", "Easy by Mac ayres", head);
-    insertAfter ("Uhaw by Dilaw", "Can We Make Love by Keith Sweat", head);
+    cout << deleteAtEnd(head);
+    traverse(head);
+    head = deleteFromBeginning(head);
+    traverse(head);
+    head = deleteFromGivenNode("Ikaw lang by Nobita",head);
     traverse(head);
     
-    
-
     return 0;
 }
+
+
